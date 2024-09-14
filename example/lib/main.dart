cff3f38b0fe5291307f550102c4e560a29af3070
Hossein <ho3einsg@gmail.com>
2024-09-14 21:37:09 +0330

    Add example

---
+++ example/lib/main.dart
@@ -0,0 +1,13 @@
+// Just add your files to stage
+// For example:
+
+/// git add .
+
+// And now just use better commit
+
+/// better-commit
+/// OR
+/// better-commit --custom
+
+// Custom flag will allow you to write your own commit message
+// and AI will just improve it and add tag and emoji to it
