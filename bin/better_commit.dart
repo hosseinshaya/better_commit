6015be0574923806eb6ecd8b7de9ebb5b2181ffc
Hossein <ho3einsg@gmail.com>
2024-09-14 14:48:57 +0330

    Instance of 'GenerateContentResponse'

--- bin/better_commit.dart
+++ bin/better_commit.dart
@@ -3,8 +3,13 @@ import 'dart:io';
 import 'package:better_commit/better_commit.dart' as better_commit;
 import 'package:dcli/dcli.dart';
 
-void main(List<String> arguments) {
-  final commitMessage = ask('📝 Enter your commit message (optional):');
-  final result = better_commit.commit(commitMessage: commitMessage);
+void main(List<String> arguments) async {
+  print('🚀 Better Commit');
+  print('🤖 Using Gemini 1.5 Flash model');
+  String? commitMessage;
+  if (arguments.contains('--custom')) {
+    commitMessage = ask('📝 Enter your commit message (optional):');
+  }
+  final result = await better_commit.commit(commitMessage: commitMessage);
   exit(result ?? 1);
 }
--- lib/better_commit.dart
+++ lib/better_commit.dart
@@ -3,6 +3,10 @@ import 'dart:io';
 import 'package:dcli/dcli.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
+/// This function is used to commit the changes to the git repository.
+/// It uses the Google Generative AI API to generate a commit
+/// message based on the changes.
+/// It then runs the git commit command with the generated message.
 Future<int?> commit({String? commitMessage}) async {
   final apiKey = env['GOOGLE_API_KEY'];
   if (apiKey == null) {
@@ -13,14 +17,18 @@ Future<int?> commit({String? commitMessage}) async {
     model: 'gemini-1.5-flash-latest',
     apiKey: apiKey,
   );
-  final result = Process.runSync('git status', []);
-  final output = result.stdout.toString();
+  final result = await Process.run('git', ['diff', '--staged']);
+  final diffOutput = result.stdout.toString();
 
-  // final response = await model.generateContent([
-  //   Content.text(
-  //       'Write a short emoji commit message based on this changes: ${run}')
-  // ]);
-  // final exitCode = run('git commit -m "$commitMessage"');
-  // return exitCode;
-  return 0;
+  final response = await model.generateContent([
+    Content.text(
+        // ignore: leading_newlines_in_multiline_strings
+        '''Write a short emoji commit message based on this changes: $diffOutput.
+        ${commitMessage == null ? '' : 'And based on this commit message: $commitMessage'}.
+        Commit message format: emoji + space + [tag.toUpperCase()] + space + commit message'''),
+  ]);
+  print('🚀 git commit -m "$response"');
+  final exitCode = run('git commit -m "$response"');
+
+  return exitCode;
 }
