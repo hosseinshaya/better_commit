daf7afc13a3712b42e419b3ff9a1297e654d13ec
Hossein <ho3einsg@gmail.com>
2024-09-14 14:46:33 +0330

    Instance of 'GenerateContentResponse'

--- bin/better_commit.dart
+++ bin/better_commit.dart
@@ -5,7 +5,6 @@ import 'package:dcli/dcli.dart';
 
 void main(List<String> arguments) {
   final commitMessage = ask('📝 Enter your commit message (optional):');
-  // print('Hello $commitMessage: ${better_commit.calculate()}!');
   final result = better_commit.commit(commitMessage: commitMessage);
   exit(result ?? 1);
 }
--- lib/better_commit.dart
+++ lib/better_commit.dart
@@ -1,6 +1,26 @@
-import 'package:dcli/dcli.dart';
+import 'dart:io';
 
-int? commit({String? commitMessage}) {
-  final exitCode = run('git commit -m "$commitMessage"');
-  return exitCode;
+import 'package:dcli/dcli.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+Future<int?> commit({String? commitMessage}) async {
+  final apiKey = env['GOOGLE_API_KEY'];
+  if (apiKey == null) {
+    print('GOOGLE_API_KEY is not set');
+    return 1;
+  }
+  final model = GenerativeModel(
+    model: 'gemini-1.5-flash-latest',
+    apiKey: apiKey,
+  );
+  final result = Process.runSync('git status', []);
+  final output = result.stdout.toString();
+
+  // final response = await model.generateContent([
+  //   Content.text(
+  //       'Write a short emoji commit message based on this changes: ${run}')
+  // ]);
+  // final exitCode = run('git commit -m "$commitMessage"');
+  // return exitCode;
+  return 0;
 }
