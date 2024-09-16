7bdea91caaddd1bc73ddb57318e6888e3a1603ee
Hossein <ho3einsg@gmail.com>
2024-09-16 12:42:56 +0330

    🎨 [REFACTOR] Remove empty line
    Remove an unnecessary empty line in `bin/better_commit.dart` to improve code readability.

--- bin/better_commit.dart
+++ bin/better_commit.dart
@@ -4,7 +4,6 @@ import 'package:better_commit/better_commit.dart' as better_commit;
 import 'package:dcli/dcli.dart';
 
 void main(List<String> arguments) async {
-  print(' ');
   print('🚀 Better Commit 🤖 Using Gemini 1.5 Flash model');
   String? commitMessage;
   if (arguments.contains('--custom')) {
